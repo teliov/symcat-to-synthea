82d18aa71d0f5e3577628615b8f519f6b699eee6
Your Name <arsene.fansi@gmail.com>
2020-03-19 16:47:06 -0400

    update tests for takinng into account advanced symptoms risk factors

--- test_generate.py
+++ test_generate.py
@@ -48,16 +48,16 @@ class TestGenerator(object):
             ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,1-4 years,http://www.symcat.com/demographics/age-1-4-years,4.2x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
             ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,5-14 years,http://www.symcat.com/demographics/age-5-14-years,1.7x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
             ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,15-29 years,http://www.symcat.com/demographics/age-15-29-years,0.7x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
-            ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,30-44 years,http://www.symcat.com/demographics/age-30-44-years,0.8x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
+            ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,30-44 years,http://www.symcat.com/demographics/age-30-44-years,0.0x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
             ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,45-59 years,http://www.symcat.com/demographics/age-45-59-years,0.5x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
             ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,60-74 years,http://www.symcat.com/demographics/age-60-74-years,0.3x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
             ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,75+ years,http://www.symcat.com/demographics/age-75-years,1.1x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
             ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,Male,http://www.symcat.com/demographics/sex-male,1.3x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
-            ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,Female,http://www.symcat.com/demographics/sex-female,0.8x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
+            ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,Female,http://www.symcat.com/demographics/sex-female,0.0x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
             ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,Black,http://www.symcat.com/demographics/race-ethnicity-black,0.9x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
             ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,Hispanic,http://www.symcat.com/demographics/race-ethnicity-hispanic,1.4x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
             ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,White,http://www.symcat.com/demographics/race-ethnicity-white,0.9x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
-            ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,Other,http://www.symcat.com/demographics/race-ethnicity-other,1.0x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
+            ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,Other,http://www.symcat.com/demographics/race-ethnicity-other,0.0x,Bleeding from ear,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
         ]
         symptoms = tmpdir.join("symptoms.csv")
         sample_data = "\n".join(sample_symptoms)
@@ -130,6 +130,38 @@ class TestGenerator(object):
         tran1 = "Simple_Transition_1"
         tran2 = "Simple_Transition_2"
         ndigits = 4
+        suff1 = "_Sym1"
+        suff2 = "_Sym2"
+
+        Ms1 = M + suff1
+        Fs1 = F + suff1
+        blas1 = black + suff1
+        hiss1 = hispa + suff1
+        whis1 = white + suff1
+        oths1 = other + suff1
+        a01s1 = a0_1 + suff1
+        a14s1 = a1_4 + suff1
+        a514s1 = a5_14 + suff1
+        a152s1 = a15_29 + suff1
+        a344s1 = a30_44 + suff1
+        a459s1 = a45_59 + suff1
+        a674s1 = a60_74 + suff1
+        a75s1 = a75 + suff1
+
+        Ms2 = M + suff2
+        Fs2 = F + suff2
+        blas2 = black + suff2
+        hiss2 = hispa + suff2
+        whis2 = white + suff2
+        oths2 = other + suff2
+        a01s2 = a0_1 + suff2
+        a14s2 = a1_4 + suff2
+        a514s2 = a5_14 + suff2
+        a152s2 = a15_29 + suff2
+        a344s2 = a30_44 + suff2
+        a459s2 = a45_59 + suff2
+        a674s2 = a60_74 + suff2
+        a75s2 = a75 + suff2
 
         modules = {
             key: generate_synthea_module(symptom_map, value)
@@ -164,8 +196,43 @@ class TestGenerator(object):
 
         assert modules[key1][state]["Symptom_1"][sym] == "Bleeding from ear"
         assert modules[key1][state][tran1][dist_dis][0][prob] == 0.53
+
+        assert Fs1 not in modules[key1][state]
+        assert modules[key1][state][Ms1][dist_dis][0][prob] == prob_val(1.3)
+
+        assert modules[key1][state][blas1][dist_dis][0][prob] == prob_val(0.9)
+        assert modules[key1][state][hiss1][dist_dis][0][prob] == prob_val(1.4)
+        assert modules[key1][state][whis1][dist_dis][0][prob] == prob_val(0.9)
+        assert modules[key1][state][oths1][dist_dis][0][prob] == prob_val(0.0)
+
+        assert not a344s1 in modules[key1][state]
+        assert modules[key1][state][a01s1][dist_dis][0][prob] == prob_val(2.1)
+        assert modules[key1][state][a14s1][dist_dis][0][prob] == prob_val(4.2)
+        assert modules[key1][state][a514s1][dist_dis][0][prob] == prob_val(1.7)
+        assert modules[key1][state][a152s1][dist_dis][0][prob] == prob_val(0.7)
+        assert modules[key1][state][a459s1][dist_dis][0][prob] == prob_val(0.5)
+        assert modules[key1][state][a674s1][dist_dis][0][prob] == prob_val(0.3)
+        assert modules[key1][state][a75s1][dist_dis][0][prob] == prob_val(1.1)
+
         assert modules[key1][state]["Symptom_2"][sym] == "Abdominal distention"
         assert modules[key1][state][tran2][dist_dis][0][prob] == 0.35
+
+        assert modules[key1][state][Ms2][dist_dis][0][prob] == prob_val(1.2)
+        assert modules[key1][state][Fs2][dist_dis][0][prob] == prob_val(0.9)
+
+        assert modules[key1][state][blas2][dist_dis][0][prob] == prob_val(0.9)
+        assert modules[key1][state][hiss2][dist_dis][0][prob] == prob_val(0.5)
+        assert modules[key1][state][whis2][dist_dis][0][prob] == prob_val(1.2)
+        assert modules[key1][state][oths2][dist_dis][0][prob] == prob_val(0.5)
+
+        assert a14s2 not in modules[key1][state]
+        assert a514s2 not in modules[key1][state]
+        assert modules[key1][state][a01s2][dist_dis][0][prob] == prob_val(0.8)
+        assert modules[key1][state][a152s2][dist_dis][0][prob] == prob_val(0.3)
+        assert modules[key1][state][a344s2][dist_dis][0][prob] == prob_val(0.8)
+        assert modules[key1][state][a459s2][dist_dis][0][prob] == prob_val(1.9)
+        assert modules[key1][state][a674s2][dist_dis][0][prob] == prob_val(2.4)
+        assert modules[key1][state][a75s2][dist_dis][0][prob] == prob_val(0.3)
 
         ##################### Test for condition 2 ############################
         assert key2 in modules
@@ -190,3 +257,20 @@ class TestGenerator(object):
 
         assert modules[key2][state]["Symptom_1"][sym] == "Abdominal distention"
         assert modules[key2][state][tran1][dist_dis][0][prob] == 0.91
+
+        assert modules[key2][state][Ms1][dist_dis][0][prob] == prob_val(1.2)
+        assert modules[key2][state][Fs1][dist_dis][0][prob] == prob_val(0.9)
+
+        assert modules[key2][state][blas1][dist_dis][0][prob] == prob_val(0.9)
+        assert modules[key2][state][hiss1][dist_dis][0][prob] == prob_val(0.5)
+        assert modules[key2][state][whis1][dist_dis][0][prob] == prob_val(1.2)
+        assert modules[key2][state][oths1][dist_dis][0][prob] == prob_val(0.5)
+
+        assert a14s2 not in modules[key2][state]
+        assert a514s2 not in modules[key2][state]
+        assert modules[key2][state][a01s1][dist_dis][0][prob] == prob_val(0.8)
+        assert modules[key2][state][a152s1][dist_dis][0][prob] == prob_val(0.3)
+        assert modules[key2][state][a344s1][dist_dis][0][prob] == prob_val(0.8)
+        assert modules[key2][state][a459s1][dist_dis][0][prob] == prob_val(1.9)
+        assert modules[key2][state][a674s1][dist_dis][0][prob] == prob_val(2.4)
+        assert modules[key2][state][a75s1][dist_dis][0][prob] == prob_val(0.3)
